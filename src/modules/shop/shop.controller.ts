58d482bc5b768152ec67184196def09701b0dff9
Arun Mohan <arunmh7@gmail.com>
2021-03-06 20:29:00 +0530

    added write and read from products json

--- src/modules/shop/shop.controller.ts
+++ src/modules/shop/shop.controller.ts
@@ -1,12 +1,15 @@
 import { RequestHandler } from 'express';
 import Product from '../../utilities/models/product.model';
 
-const getProducts: RequestHandler = (req, res, next) => {
+const getProducts: RequestHandler = async (req, res, next) => {
+
+    const products = await Product.fetchAll();
+
     res.render('shop', {
-        prods: Product.fetchAll(),
+        prods: products,
         pageTitle: 'Shop',
         path: '/',
-        hasProducts: Product.fetchAll().length > 0,
+        hasProducts: products.length > 0,
         activeShop: true,
         productCSS: true
     });
--- src/utilities/models/product.model.ts
+++ src/utilities/models/product.model.ts
@@ -1,8 +1,11 @@
 
+import path from 'path';
+import fs from 'fs';
+
+import rootDir from '../helpers/path';
 import { IProduct } from '../interfaces/product.interface';
 
 class Product {
-    private static products: IProduct[] = [];
 
     title = '';
 
@@ -11,11 +14,37 @@ class Product {
     }
 
     save() {
-        Product.products.push(this);
+
+        const p = path.join(rootDir, 'src', 'data', 'products.json');
+
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent.toString());
+            }
+            products.push(this);
+
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return Product.products;
+    static fetchAll(): Promise<Product[]> {
+        const p = path.join(rootDir, 'src', 'data', 'products.json');
+
+        const promise = new Promise<Product[]>((resolve, reject) => {
+            fs.readFile(p, (err, fileContent) => {
+                let products = [];
+                if (!err) {
+                    products = JSON.parse(fileContent.toString());
+                    resolve(products);
+                }
+                resolve([]);
+            });
+        });
+
+        return promise;
     }
 }
 
