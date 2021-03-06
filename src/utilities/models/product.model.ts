58faa326aea7720f8ecdb31cbf5225a7c83582d5
Arun Mohan <arunmh7@gmail.com>
2021-03-06 20:41:53 +0530

    product model refactor

--- src/utilities/models/product.model.ts
+++ src/utilities/models/product.model.ts
@@ -5,6 +5,19 @@ import fs from 'fs';
 import rootDir from '../helpers/path';
 import { IProduct } from '../interfaces/product.interface';
 
+const productsPath = path.join(rootDir, 'src', 'data', 'products.json');
+
+const readProdJson = new Promise<Product[]>((resolve, reject) => {
+    fs.readFile(productsPath, (err, fileContent) => {
+        let products = [];
+        if (!err) {
+            products = JSON.parse(fileContent.toString());
+            resolve(products);
+        }
+        resolve([]);
+    });
+});
+
 class Product {
 
     title = '';
@@ -13,38 +26,18 @@ class Product {
         this.title = product.title;
     }
 
-    save() {
+    async save() {
+        const products = await readProdJson;
 
-        const p = path.join(rootDir, 'src', 'data', 'products.json');
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent.toString());
-            }
-            products.push(this);
-
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
+        products.push(this);
+        fs.writeFile(productsPath, JSON.stringify(products), (err) => {
+            console.log(err);
         });
+
     }
 
     static fetchAll(): Promise<Product[]> {
-        const p = path.join(rootDir, 'src', 'data', 'products.json');
-
-        const promise = new Promise<Product[]>((resolve, reject) => {
-            fs.readFile(p, (err, fileContent) => {
-                let products = [];
-                if (!err) {
-                    products = JSON.parse(fileContent.toString());
-                    resolve(products);
-                }
-                resolve([]);
-            });
-        });
-
-        return promise;
+        return readProdJson;
     }
 }
 
