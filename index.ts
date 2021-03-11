d1e07e41136a43fe040ac4f29068411e0ef4dd44
Arun Mohan <arunmh7@gmail.com>
2021-03-11 11:32:22 +0530

    db and index file changes

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import path from 'path';
 
-// import adminRouter from './src/modules/admin/admin.routes';
-// import shopRouter from './src/modules/shop/shop.routes';
+import adminRouter from './src/modules/admin/admin.routes';
+import shopRouter from './src/modules/shop/shop.routes';
 import HttpStatus from './src/utilities/enums/http-status.enum';
 import client from './src/utilities/helpers/database';
 import rootDir from './src/utilities/helpers/path';
@@ -15,16 +15,17 @@ app.set('views', 'src/views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-// app.use('/admin', adminRouter);
-// app.use(shopRouter);
+app.use('/admin', adminRouter);
+app.use(shopRouter);
 
 app.use("*", (req, res) => {
     res.status(HttpStatus.NOT_FOUND).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
-client
-    .then(client => {
-        console.log(client);
-        app.listen(8000);
-    })
-    .catch(err => console.log(err))
+client()
+    .then((success) => {
+        if (success) {
+            console.log(`⚡️[server]: Server is running at https://localhost:8000`);
+            app.listen(8000);
+        }
+    });
--- src/utilities/helpers/database.ts
+++ src/utilities/helpers/database.ts
@@ -1,7 +1,26 @@
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
 const URI = "mongodb+srv://root:admin%40123@cluster0.kajhf.mongodb.net/test?retryWrites=true&w=majority";
 
-const mongoClient = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true }).connect();
+let _db: Db;
+
+const mongoClient = async () => {
+    try {
+        const client = await new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true }).connect();
+        _db = client.db();
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+export const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw new Error("No database found");
+}
+
+
 
 export default mongoClient;
