9ea681d8e422a50f1ad87a0221bdd7af9bf26879
Arun Mohan <arunmh7@gmail.com>
2021-03-11 11:53:10 +0530

    edit and delete fn added

--- src/utilities/models/product.model.ts
+++ src/utilities/models/product.model.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb';
+import { Db, ObjectId } from 'mongodb';
 import { getDb } from '../helpers/database';
 import { IProduct } from '../interfaces/product.interface';
 
@@ -7,16 +7,25 @@ class Product {
     price: number;
     description: string;
     imageUrl: string;
+    id?: string | null;
     constructor(product: IProduct) {
         this.title = product.title;
         this.price = product.price;
         this.description = product.description;
         this.imageUrl = product.imageUrl;
+        this.id = product.id;
     }
 
     save() {
-        const db = getDb();
-        return db.collection('products').insertOne(this);
+        if (this.id) {
+            return getDb().collection('products').updateOne({ _id: new ObjectId(this.id) }, { $set: this })
+        } else {
+            return getDb().collection('products').insertOne(this);
+        }
+    }
+
+    static delete(id: string) {
+        return getDb().collection('products').deleteOne({ _id: new ObjectId(id) });
     }
 
     static fetchAll() {
